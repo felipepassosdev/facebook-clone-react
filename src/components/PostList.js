86b1b83b782174644d855b9fa73183c1662fae1a
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-05 20:02:28 -0300

    PostList

---
+++ src/components/PostList.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+
+import PostItem from './PostItem';
+
+class PostList extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: 'Felipe Passos',
+          avatar: 'https://scontent.fcgh37-1.fna.fbcdn.net/v/t1.0-9/s960x960/85236448_2928900133807199_2758220962334769152_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=ViZArqZQghAAX82jnFU&_nc_ht=scontent.fcgh37-1.fna&_nc_tp=7&oh=7e0566ff7967824245c05f3f386c553b&oe=5ED761C8'
+        },
+        date: '4 de Maio 2019 às 15:47',
+        content: 'Pessoal, alguém sabe se a Rocketseat está contratando?',
+        image: 'https://scontent.fcgh37-1.fna.fbcdn.net/v/t1.0-9/p960x960/48391425_366676570545490_3560191485070540800_o.jpg?_nc_cat=100&_nc_sid=2d5d41&_nc_ohc=gapZh43hOa8AX9AvBw8&_nc_ht=scontent.fcgh37-1.fna&_nc_tp=6&oh=1768e7b957885a57ed2dae5edacaf3d1&oe=5ED8F1FC',
+        comments: [
+          {
+            id: 2,
+            author: {
+              name: 'Diego Fernandes',
+              avatar: 'https://avatars2.githubusercontent.com/u/2254731?v=4'
+            },
+            date: '04 Jun 2019',
+            content:
+              'A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)'
+          }
+        ]
+      },
+      {
+        id: 3,
+        author: {
+          name: 'Neil Cook',
+          avatar: 'https://i.pravatar.cc/150?img=8'
+        },
+        date: '04 Jun 2019',
+        content:
+          'Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia',
+        comments: [
+          {
+            id: 4,
+            author: {
+              name: 'Clara Lisboa',
+              avatar: 'https://i.pravatar.cc/150?img=5'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!'
+          },
+          {
+            id: 5,
+            author: {
+              name: 'Cézar Toledo',
+              avatar: 'https://i.pravatar.cc/150?img=11'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes'
+          }
+        ]
+      },
+      {
+        id: 6,
+        author: {
+          name: 'Gabriel Lisboa',
+          avatar: 'https://i.pravatar.cc/150?img=51'
+        },
+        date: '04 Jun 2019',
+        content:
+          'Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia',
+        comments: [
+          {
+            id: 4,
+            author: {
+              name: 'Clara Lisboa',
+              avatar: 'https://i.pravatar.cc/150?img=5'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!'
+          },
+          {
+            id: 5,
+            author: {
+              name: 'Cézar Toledo',
+              avatar: 'https://i.pravatar.cc/150?img=11'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes'
+          }
+        ]
+      }
+    ]
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="postlist">
+        {posts.map(post => (
+          <PostItem key={post.id} {...post} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default PostList;
